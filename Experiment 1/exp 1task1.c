91e78ac7d22f15e9a62d9cfa7ff62dfeba303986
Mohsin4950 <mohsinsidhpurwala@eng.rizvi.edu.in>
2024-10-23 19:23:52 +0530

    Add files via upload

---
+++ Experiment 1/exp 1task1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+printf("Full name\t: Mohsin Ismail Sidhpurwala\n");
+printf("Age\t\t: 18\n");
+printf("Class\t\t: FE -Aids\n");
+printf("division\t: A\n");
+printf("UIN\t\t: 241A032");
+return 0;
+
+}
